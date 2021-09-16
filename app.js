3ed840747bbbbf69dd24e7c328c33dc287bf4b0e
sanjibdey104 <sanjibdey.dey4@gmail.com>
2021-09-16 11:13:47 +0530

    feat: show trending gif, search gif, insert in post form

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
 const gifSearchPopupBtn = document.querySelector("#gif-search_popup-btn");
 const gifSearchPopup = document.querySelector(".gif-search_popup");
 const gifSearchInput = document.querySelector("#gif-search_input");
-const gifFetchBtn = document.querySelector("#fetch-gif-btn");
+const gifSearchResults = document.querySelector(".gif-search_results");
+const postTextInput = document.querySelector("#post-text-input");
+
+const giphyApiKey = "Xnxm7Myd7Kq7YrJgWupPTiqr9cGyVfyK";
+const giphyTrendingEndpoint = `https://api.giphy.com/v1/gifs/trending?api_key=${giphyApiKey}&limit=1`;
+
+const renderGifs = (data) => {
+  gifSearchResults.innerHTML = "";
+  const gifTitle = data.title;
+  const gifUrl = data.images.downsized.url;
+  const gifImage = document.createElement("img");
+  gifImage.classList.add("gif-search_result-img");
+  gifImage.setAttribute("src", gifUrl);
+  gifImage.setAttribute("alt", gifTitle);
+  gifSearchResults.appendChild(gifImage);
+};
+
+const fetchTrendingGifs = async () => {
+  try {
+    const res = await fetch(giphyTrendingEndpoint);
+    const content = await res.json();
+    console.log(content);
+    renderGifs(content.data[0]);
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 gifSearchPopupBtn.addEventListener("click", function () {
   if (gifSearchPopup.classList.contains("show-popup")) {
@@ -12,28 +38,41 @@ gifSearchPopupBtn.addEventListener("click", function () {
   }
 });
 
-const giphyApiKey = "Xnxm7Myd7Kq7YrJgWupPTiqr9cGyVfyK";
-const giphyTrendingEndpoint = `https://api.giphy.com/v1/gifs/trending?api_key=${giphyApiKey}&=1`;
-let giphySearchEndpoint = `https://api.giphy.com/v1/gifs/search?api_key=${giphyApiKey}&limit=1&q=`;
+const fetchSearchedGifs = async (e) => {
+  const searchQuery = e.target.value;
+  console.log(searchQuery);
 
-gifFetchBtn.addEventListener("click", fetchSearchedGifs);
-
-function fetchSearchedGifs(e) {
-  e.preventDefault();
-  const searchInput = gifSearchInput.value.trim();
-
-  if (searchInput) {
-    giphySearchEndpoint = giphySearchEndpoint.concat(searchInput);
-    fetch(giphySearchEndpoint)
-      .then((res) => res.json())
-      .then((content) => console.log(content))
-      .catch((err) => console.log(err));
+  if (searchQuery) {
+    let giphySearchEndpoint = `https://api.giphy.com/v1/gifs/search?api_key=${giphyApiKey}&limit=1&q=${searchQuery}`;
+    try {
+      const res = await fetch(giphySearchEndpoint);
+      const content = await res.json();
+      renderGifs(content.data[0]);
+    } catch (err) {
+      console.log(err);
+    }
+  } else {
+    fetchTrendingGifs();
   }
-}
+};
 
-function fetchTrendingGifs() {
-  fetch(giphyTrendingEndpoint)
-    .then((res) => res.json())
-    .then((content) => console.log(content))
-    .catch((err) => console.log(err));
-}
+gifSearchInput.addEventListener("keyup", fetchSearchedGifs);
+
+const insertGifInPost = (e) => {
+  if (e.target.classList.contains("gif-search_result-img")) {
+    console.dir(e.target);
+    const gifTitle = e.target.alt;
+    const gifUrl = e.target.src;
+    const selectedGifImage = document.createElement("img");
+    selectedGifImage.classList.add("selected-gif-img");
+    selectedGifImage.setAttribute("src", gifUrl);
+    selectedGifImage.setAttribute("alt", gifTitle);
+    postTextInput.parentNode.insertBefore(
+      selectedGifImage,
+      postTextInput.nextSibling
+    );
+    gifSearchPopup.classList.remove("show-popup");
+  }
+};
+
+document.addEventListener("click", insertGifInPost);
